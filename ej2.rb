df92c3eccbcee146e080bf3268a9e95e9cab7397
tam <tamara.acevedoavila@gmail.com>
2018-05-04 20:20:30 -0300

    ejercicio 2

--- ej2.rb
+++ ej2.rb
@@ -1,1 +1,16 @@
-nombres = ["Violeta", "Andino", "Clemente", "Javiera", "Paula", "Pia", "Ray"]
+nombres = ["Violeta", "Andino", "Clemente", "Javiera", "Paula", "Pia", "Ray", "uPa"]
+
+uno = nombres.select { |v| v.length > 5}
+puts "nombres mayores a 5 #{uno}"
+
+dos = nombres.map { |v| v.downcase}
+puts "nombres minuscula #{dos}"
+
+tres = nombres.select { |v| v[0].include?('P')}
+puts "nombres con P #{tres}"
+
+cuatro = nombres.map { |v| v.length}
+puts "nombres con P #{cuatro}"
+
+cinco = nombres.map { |v| v.gsub(/[aeiou]/, '') }
+puts "nombres con P #{cinco}"
